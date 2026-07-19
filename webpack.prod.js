905eb6e62cfb21e154f21bb7c0dde7957adecc97
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-07-20 00:07:15 +0600

    webpack-merge

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,8 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-export default {
+import common from "./webpack.common.js";
+import { merge } from "webpack-merge";
+export default merge(common, {
   mode: "production",
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+  devtool: "source-map",
+});
